a0efdfb04dc81547a0717db0b338a7191e4e5db5
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-04-19 19:53:32 -0500

    Uploaded the source directory

---
+++ LinuxNativeJam/Source_Code/Character.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<string>
+class Character
+{
+private:
+   int playerOccupation; 
+public:
+    Character(int PlayerOccupation);
+    ~Character();
+};
+
+Character::Character(int PlayerOccupation)
+{
+}
+
+Character::~Character()
+{
+}
---
+++ LinuxNativeJam/Source_Code/Game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<Character.h>
+
+// functions
+
+void PlayGame()
+{
+std::cout<<"It is a bright sunny day in June of 1884, and you have just gotten done with your work for the day.\nWhat is it that you do for a living?";
+std::cout<<"1) a blacksmith, who can get a discount on materials\n 2) a";
+int playerOccupation;
+std::cin>> playerOccupation;
+switch (playerOccupation)
+{
+case 1:
+    /* code */
+    break;
+
+default:
+    break;
+}
+}
+
+// main program function
+int main()
+{
+    PlayGame();
+}
