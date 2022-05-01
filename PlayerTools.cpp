4ac91eb11a6327287b4e83db4153b1200ec9cc89
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-05-01 00:49:46 -0500

    Resolved Issue #1

---
+++ PlayerTools.cpp
@@ -0,0 +1,17 @@
+#include "PlayerTools.h"
+class PlayerTools
+{
+private:
+    /* data */
+public:
+    PlayerTools(/* args */);
+    ~PlayerTools();
+};
+
+PlayerTools::PlayerTools(/* args */)
+{
+}
+
+PlayerTools::~PlayerTools()
+{
+}
